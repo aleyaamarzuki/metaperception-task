dd5daca74bc6805fade54e63ec2e0b12058c9a0e
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2024-04-24 13:10:50 +0200

    Update PerTask.js

--- src/Components/PerTask.js
+++ src/Components/PerTask.js
@@ -90,7 +90,7 @@ class PerTask extends React.Component {
 
     var PlayerProbsOrder = PlayerProbs[Math.floor(Math.random() * 7)];
     
-    var orderValue = 1//PlayerProbs.indexOf(PlayerProbsOrder);
+    var orderValue = 1;//PlayerProbs.indexOf(PlayerProbsOrder);
 
     
     //////////////////////////////////////////////////////////////////////////////////////////////
