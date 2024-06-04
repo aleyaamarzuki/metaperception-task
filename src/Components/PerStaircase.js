2b3088fd759ca15e265ffc2b0f50a87eff9b2df4
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2024-06-04 12:00:27 +0200

    Update PerStaircase.js

--- src/Components/PerStaircase.js
+++ src/Components/PerStaircase.js
@@ -10,48 +10,34 @@ function checkReversal(dir) {
 }
 
 export function staircase(dotDiff, prevTrialPerf, dir, trialNum) {
-  var back1 = prevTrialPerf[prevTrialPerf.length - 1]; // Last trial
-  var back2 = prevTrialPerf[prevTrialPerf.length - 2]; // Two trials ago
-  var reverse = false; // Initialize reversal to false
+  var back1 = prevTrialPerf[prevTrialPerf.length - 1];
+  var back2 = prevTrialPerf[prevTrialPerf.length - 2];
+  var reverse = false;
 
   if (back1) {
     // If the last trial was correct
     if (back2) {
-      // AND two trials ago were correct
-      if (trialNum < 7) dotDiff -= 0.4;
-      // for 0==trial and for the second half of the practice
-      else if (trialNum > 6 && trialNum < 12) dotDiff -= 0.2;
-      // for the first 5 trials of the practice
-      else if (trialNum > 11) dotDiff -= 0.1;
-      // for next 5 trials of the practice
-
-      // changes the last trial to incorrect
-      // so if previous two trials were correct and the staircase increased difficulty
-      // it needs two more trials again before it lowers again in value
+      // Decrease difficulty for correct trials
+      if (trialNum < 7) dotDiff -= 0.2;
+      else if (trialNum > 6 && trialNum < 12) dotDiff -= 0.1;
+      else if (trialNum > 11) dotDiff -= 0.05;
       prevTrialPerf[prevTrialPerf.length - 1] = false;
-
-      dir[0] = dir[1]; // Set the direction two trials ago to the direction one trial ago
-      dir[1] = "up"; // Set the direction one trial ago to up
-      reverse = checkReversal(dir); // Check if there was a reversal in direction as a result of the step down
+      dir[0] = dir[1];
+      dir[1] = "up";
+      reverse = checkReversal(dir);
     }
-    // If the last trial was correct and two trials ago were wrong, do nothing.
-  } // If the last trial was wrong
-  else {
-    if (trialNum < 7) dotDiff += 0.4;
-    // for 0==trial and for the second half of the practice
-    else if (trialNum > 6 && trialNum < 12) dotDiff += 0.2;
-    // for the first 5 trials of the practice
-    else if (trialNum > 11) dotDiff += 0.1;
-    // for next 5 trials of the practice
-
-    dir[0] = dir[1]; // Set the direction two trials ago to the direction one trial ago
-    dir[1] = "down"; // Set the direction one trial ago to down
-    reverse = checkReversal(dir); // Check if there was a reversal in direction as a result of the step up
+  } else {
+    // If the last trial was wrong
+    // Increase difficulty for incorrect trials
+    if (trialNum < 7) dotDiff += 0.2;
+    else if (trialNum > 6 && trialNum < 12) dotDiff += 0.1;
+    else if (trialNum > 11) dotDiff += 0.05;
+    dir[0] = dir[1];
+    dir[1] = "down";
+    reverse = checkReversal(dir);
   }
 
-  // Set limits on dots_diff s.t. it remains in the range of [0,50] inclusive
-  //if (dots_diff >= 4.25)
-  //  dots_diff = 4.25;
+  // Set limits on dotDiff to remain in the range [0, 50]
   if (dotDiff <= 1) dotDiff = 1;
 
   var output = {
@@ -63,3 +49,4 @@ export function staircase(dotDiff, prevTrialPerf, dir, trialNum) {
 
   return output;
 }
+
