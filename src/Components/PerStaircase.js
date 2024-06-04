4a3dbc7d54fbaae665f363eeff695e4cd6907ebe
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2024-06-04 14:23:15 +0200

    Update PerStaircase.js

--- src/Components/PerStaircase.js
+++ src/Components/PerStaircase.js
@@ -9,35 +9,89 @@ function checkReversal(dir) {
   else return false;
 }
 
+var curInstructNum = this.state.instructNum;
+    var whichButton = keyPressed;
+
+    if (whichButton === 3 && curInstructNum === 4) {
+      const results1 = [];
+      const results2 = [];
+      const results3 = [];
+
+
+    for (let i = 0; i < this.state.trialNumPerBlock; i++) {
+        
+        const randomValue = Math.random();
+
+        if (randomValue < this.state.PlayerProbsOrder[0]) {
+            results1.push(1);
+        } else {
+            results1.push(0);
+        }
+    }
+    for (let i = 0; i < this.state.trialNumPerBlock; i++) {
+        
+      const randomValue = Math.random();
+
+      if (randomValue < this.state.PlayerProbsOrder[1]) {
+          results2.push(1);
+      } else {
+          results2.push(0);
+      }
+  }
+  for (let i = 0; i < this.state.trialNumPerBlock; i++) {
+        
+    const randomValue = Math.random();
+
+    if (randomValue < this.state.PlayerProbsOrder[2]) {
+        results3.push(1);
+    } else {
+        results3.push(0);
+    }
+}
+
 export function staircase(dotDiff, prevTrialPerf, dir, trialNum) {
-  var back1 = prevTrialPerf[prevTrialPerf.length - 1];
-  var back2 = prevTrialPerf[prevTrialPerf.length - 2];
-  var reverse = false;
+  var back1 = prevTrialPerf[prevTrialPerf.length - 1]; // Last trial
+  var back2 = prevTrialPerf[prevTrialPerf.length - 2]; // Two trials ago
+  var reverse = false; // Initialize reversal to false
 
   if (back1) {
     // If the last trial was correct
     if (back2) {
-      // Decrease difficulty for correct trials
-      if (trialNum < 7) dotDiff -= 0.2;
-      else if (trialNum > 6 && trialNum < 12) dotDiff -= 0.1;
-      else if (trialNum > 11) dotDiff -= 0.05;
+      // AND two trials ago were correct
+      if (trialNum < 7) dotDiff -= 0.8;
+      // for 0==trial and for the second half of the practice
+      else if (trialNum > 6 && trialNum < 12) dotDiff -= 0.4;
+      // for the first 5 trials of the practice
+      else if (trialNum > 11) dotDiff -= 0.2;
+      // for next 5 trials of the practice
+
+      // changes the last trial to incorrect
+      // so if previous two trials were correct and the staircase increased difficulty
+      // it needs two more trials again before it lowers again in value
       prevTrialPerf[prevTrialPerf.length - 1] = false;
-      dir[0] = dir[1];
-      dir[1] = "up";
-      reverse = checkReversal(dir);
+
+      dir[0] = dir[1]; // Set the direction two trials ago to the direction one trial ago
+      dir[1] = "up"; // Set the direction one trial ago to up
+      reverse = checkReversal(dir); // Check if there was a reversal in direction as a result of the step down
     }
-  } else {
-    // If the last trial was wrong
-    // Increase difficulty for incorrect trials
-    if (trialNum < 7) dotDiff += 0.2;
-    else if (trialNum > 6 && trialNum < 12) dotDiff += 0.1;
-    else if (trialNum > 11) dotDiff += 0.05;
-    dir[0] = dir[1];
-    dir[1] = "down";
-    reverse = checkReversal(dir);
+    // If the last trial was correct and two trials ago were wrong, do nothing.
+  } // If the last trial was wrong
+  else {
+    if (trialNum < 7) dotDiff += 0.8;
+    // for 0==trial and for the second half of the practice
+    else if (trialNum > 6 && trialNum < 12) dotDiff += 0.4;
+    // for the first 5 trials of the practice
+    else if (trialNum > 11) dotDiff += 0.2;
+    // for next 5 trials of the practice
+
+    dir[0] = dir[1]; // Set the direction two trials ago to the direction one trial ago
+    dir[1] = "down"; // Set the direction one trial ago to down
+    reverse = checkReversal(dir); // Check if there was a reversal in direction as a result of the step up
   }
 
-  // Set limits on dotDiff to remain in the range [0, 50]
+  // Set limits on dots_diff s.t. it remains in the range of [0,50] inclusive
+  //if (dots_diff >= 4.25)
+  //  dots_diff = 4.25;
   if (dotDiff <= 1) dotDiff = 1;
 
   var output = {
