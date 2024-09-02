11db6076bb4252533e236c1e01740f5ce3dd2f35
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2024-09-02 10:23:46 +0200

    Create consentTueb.js

---
+++ src/Components/consent/consentTueb.js
@@ -0,0 +1,179 @@
+
+// Full consent form version
+
+export const json = {
+  title: null,
+  pages: [
+    {
+      questions: [
+        { type: "html", name: "info", html: "" },
+        {
+          type: "html",
+          name: "info",
+          html: "<b>Who is conducting this research study?</b>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p>This research is being conducted by the Department of Psychiatry and Psychotherapy, Universitätsklinikum Tübingen. The lead researchers for this project are Dr. Aleya Marzuki (Research Fellow, aleya.marzuki@uni-tuebingen.de) and Prof. Tobias Hauser (Principal Investigator, tobias.hauser@uni-tuebingen.de). This study has been approved by the Universitätsklinikum Tübingen Ethics Committee (project ID number 336/2024BO1).</p>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html: "<b>What is the purpose of this study?</b>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p>We are interested in how the brain controls learning and decision-making. This research aims to provide insights into how the healthy brain works to help us understand the causes of a number of different medical conditions.</p>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html: "<b>Who can participate in the study?</b>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p>People aged between <strong>13 to 60 years old</strong>, are <strong>fluent in German or English</strong>, and have <strong>normal or corrected-to-normal vision</strong>. If you take part in this study, you confirm that you meet the eligibity criteria.</p>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html: "<b>What will happen to me if I take part?</b>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p>You will play an online computer task, which will last around approximately <strong>30 minutes</strong>. <br/><br/>You will receive <strong>€8</strong> for helping us.<br/>Plus, depending on your performance you can earn an <strong>additional bonus</strong> of up to <strong>€3</strong>. <br/><br/>Remember, you are free to withdraw at any time without giving a reason.</p>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<b>What are the possible disadvantages and risks of taking part?</b>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p> The task you complete does not pose any known risks.</p>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html: "<b>What are the possible benefits of taking part?</b>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p>While there are no immediate benefits to taking part, your participation in this research will help us understand how people make decisions and this could have benefits for our understanding of mental health problems.</p>",
+        },
+
+        { type: "html", name: "info", html: "<b>Complaints</b>" },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p>If you wish to complain or have any concerns about any aspect of the way you have been approached or treated by members of staff, then the Data Protection Officer can assist. In the first instance, please talk to the researcher or the chief investigator (Prof Tobias Hauser, tobias.hauser@uni-tuebingen.de) about your complaint. If you feel that the complaint has not been resolved satisfactorily, please contact the Data Protection Officer at Universitätsklinikum Tübingen (dsb@med.uni-tuebingen.de; 07071 29-87667).</p>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html: "<b>What about my data?</b>",
+        },
+
+        {
+          type: "html",
+          name: "info",
+          html:
+            "<p>To help future research and make the best use of the research data you have given us (such as answers to questionnaires) we may keep your research data indefinitely and share these. The data we collect will be shared and held as follows:<ul><li> In publications, your data will be anonymised, so you cannot be identified. </li><li> In public databases, your data will be anonymised. </li><li>We do not collect any personal data that could be used to identify you. </li><li> Personal data is any information that could be used to identify you, such as your User ID. When we collect your data, your User ID will be replaced with a nonidentifiable random ID number. No personally identifying data will be stored.</li></ul>The legal basis used to process your personal data will be the provision of public task (this means that the research you are taking part in is deemed to be in the public interest). The legal basis used to process special category data (i.e. ethnicity) will be for scientific research purposes. We will follow the university and legal guidelines to safeguard your data. If you change your mind and withdraw your consent to participate in this study you can contact us via Prolific. However, we collect all data in an anonymised form, which is why this data cannot be destroyed, withdrawn or recalled. <br/><br/> Your data protection rights are protected under Articles 12ff of the European General Data Protection Regulation (EU-GDPR). If there are any queries or concerns please do not hesitate to contact Dr. Aleya Marzuki (aleya.marzuki@uni-tuebingen.de).</p>",
+        },
+      ],
+    },
+    {
+      questions: [
+        {
+          type: "checkbox",
+          name: "checkbox1",
+          title:
+            "I have read the information on the previous page, and understand what the study involves.",
+          isRequired: true,
+          choices: ["Yes"],
+        },
+
+        {
+          type: "checkbox",
+          name: "checkbox2",
+          title:
+            "I consent to the processing of my personal information (e.g. User ID) for the purposes of this research study. I understand that such information will remain confidential and will be handled in accordance with all applicable data protection legislation and ethical standards in research. These data will only be accessible to the study team and individuals from the University and Funder who are responsible for monitoring and audits.",
+          isRequired: true,
+          choices: ["Yes"],
+        },
+
+        {
+          type: "checkbox",
+          name: "checkbox3",
+          title:
+            "I understand that my anonymised personal data can be shared with others for future research, shared in public databases and in scientific reports.",
+          isRequired: true,
+          choices: ["Yes"],
+        },
+
+        {
+          type: "checkbox",
+          name: "checkbox4",
+          title:
+            "I understand that I am free to withdraw from this study at any time without giving a reason and this will not affect my future medical care or legal rights.",
+          isRequired: true,
+          choices: ["Yes"],
+        },
+
+        {
+          type: "checkbox",
+          name: "checkbox5",
+          title:
+            "I understand the potential benefits and risks of participating, the support available to me should I become distressed during the research, and who to contact if I wish to lodge a complaint.",
+          isRequired: true,
+          choices: ["Yes"],
+        },
+
+        {
+          type: "checkbox",
+          name: "checkbox6",
+          title:
+            "I understand the inclusion and exclusion criteria in the Information Sheet and as explained to me by the researcher.  I confirm that I do not fall under the exclusion criteria.",
+          isRequired: true,
+          choices: ["Yes"],
+        },
+
+        {
+          type: "checkbox",
+          name: "checkbox7",
+          title:
+            "I agree that the research project named on the previous page has been explained to me to my satisfaction and I agree to take part in this study.",
+          isRequired: true,
+          choices: ["Yes"],
+        },
+      ],
+    },
+  ],
+};
